1fd103c8bc5fcb3a1abfaf878763a0f42cee65e2
Nanorooter <nanorooter@gmail.com>
2021-05-13 13:52:09 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_recipe/home_page.dart';
+import 'package:flutter_recipe/pages/home_page.dart';
+import 'package:flutter_recipe/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Homepage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      // home: Homepage(),
+      routes: {
+        "/": (context) => Homepage(),
+        "/home": (context) => Homepage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
